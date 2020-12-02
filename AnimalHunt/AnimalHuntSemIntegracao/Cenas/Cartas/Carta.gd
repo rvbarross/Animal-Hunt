ff17f48e2ba92580976bd3f7b733e31495f37cbb
JLMSC <39315631+JLMSC@users.noreply.github.com>
2020-12-02 19:36:40 -0300

    Update
    Adicionado um Menu, música de fundo pro menu, modo de jogo infinito, arrumado o acerto dos pares das cartas, hud no jogo, pontuação, sprite guardado para a palavar "Parabéns" smp q o usuário terminar uma fase, nome do animal, imagem do animal, descricao do animal, botões para reproduzir o som do animal e a pronuncia do nome dele em ingles, arrumado as imagens e o nome dos animais e foi diminuido o tamanho do sprite nas cartas (ao virar); removido os arquivos antigos, ja q esse é uma junção deles

---
+++ AnimalHunt/AnimalHuntSemIntegracao/Cenas/Cartas/Carta.gd
@@ -0,0 +1,23 @@
+extends Button
+
+# O animal que a carta está segurando.
+var animal = ""
+var pressionado = false
+
+func _on_Carta_pressed():
+	#print(animal)
+	
+	if !pressionado:
+		pressionado = true
+	
+		# Adiciona a carta às cartas selecionadas.
+		get_parent().cartas_selecionadas.append(self)
+		
+		# Verifica a carta e atribui valor.
+		if get_parent().primeira_carta == null:
+			get_parent().primeira_carta = animal
+		elif get_parent().segunda_carta == null:
+			get_parent().segunda_carta = animal
+		
+		$AnimationPlayer.play("revelar_carta")
+		$animal.texture = load("res://Animais/%s.tres" % animal)
